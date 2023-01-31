3efefa740e46a86307af9218c6af751812243f81
Guiatsu <yuricrystian@hotmail.com>
2023-01-30 21:10:11 -0300

    feat: game menu created

---
+++ Assets/Scripts/MainMenu.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour
+{
+    [SerializeField] public GameObject InstructionsPanel;
+    [SerializeField] public GameObject MainMenuPanel;
+    public void PlayGame()
+    {
+        SceneManager.LoadScene("Gameplay");
+    }
+    public void ShowInstructions(){
+        InstructionsPanel.SetActive(true);
+        MainMenuPanel.SetActive(false);
+    }
+    public void HideInstructions(){
+        InstructionsPanel.SetActive(false);
+        MainMenuPanel.SetActive(true);
+    }
+}
